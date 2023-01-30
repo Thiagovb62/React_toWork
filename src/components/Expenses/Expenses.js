3432af3fee41eeb51c36d038ebed76d84ccb3173
thiagovb62@gmail.com <thiagovb62@gmail.com>
2023-01-30 17:09:47 -0300

    Child-to-Parent chalenge date

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,27 +1,34 @@
 import ExpenseItem from "../ExpensesItem/ExpenseItem";
-import React from "react";
+import React, {useState} from "react";
 import './Expenses.css';
 import Card from "../UI/Card";
+import ExpensesFilter from "./ExpensesFilter";
 const Expenses = (props) => {
-
+    const [filteredYear, setFilteredYear] = useState('2020');
+    const filterChangeHandler = selectedYear => {
+       setFilteredYear(selectedYear)
+    }
     return (
-        <Card className="expenses">
-            <ExpenseItem
-                title={props.items[0].title}
-                amount={props.items[0].amount}
-                date={props.items[0].date}
-            />
-            <ExpenseItem
-                title={props.items[1].title}
-                amount={props.items[1].amount}
-                date={props.items[1].date}
-            />
-            <ExpenseItem
-                title={props.items[2].title}
-                amount={props.items[2].amount}
-                date={props.items[2].date}
-            />
-        </Card>
+        <div>
+            <Card className="expenses">
+            <ExpensesFilter selectedYear={filteredYear} onChangeFilter={filterChangeHandler} />
+                <ExpenseItem
+                    title={props.items.at(0).title}
+                    amount={props.items.at(0).amount}
+                    date={props.items.at(0).date}
+                />
+                <ExpenseItem
+                    title={props.items.at(1).title}
+                    amount={props.items.at(1).amount}
+                    date={props.items.at(1).date}
+                />
+                <ExpenseItem
+                    title={props.items.at(2).title}
+                    amount={props.items.at(2).amount}
+                    date={props.items.at(2).date}
+                />
+            </Card>
+        </div>
     )
     // return ( React.createElement(Card, {className: "expenses"},props.children),
     //     React.createElement(ExpenseItem, {title: props.items[0].title, amount: props.items[0].amount, date: props.items[0].date}),
