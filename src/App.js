ae45ff69f9b6ae57d7c33c246e04d79bd9df3e71
thiagovb62@gmail.com <thiagovb62@gmail.com>
2023-02-02 16:51:37 -0300

    create a example page and Using the useEffect() Hook

--- src/App.js
+++ src/App.js
@@ -1,14 +1,40 @@
-import './App.css';
-import React, {useState,fragment} from 'react';
-
-
+import React, { useState } from 'react';
+import {useEffect} from "react";
+import Login from './components/Login/Login';
+import Home from './components/Home/Home';
+import MainHeader from './components/MainHeader/MainHeader';
 
 function App() {
 
+    const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  useEffect(() => {
+      const storedUserLoggedInInformations = localStorage.setItem('isLoggedIn', '0');
+
+      if (storedUserLoggedInInformations === '1') {
+        setIsLoggedIn(true);
+        }
+      },[])
+   const loginHandler = (email, password) => {
+        // We should of course check email and password
+        // But it's just a dummy/ demo anyways
+        localStorage.setItem('isLoggedIn', '1');
+        setIsLoggedIn(true);
+    };
+
+    const logoutHandler = () => {
+        localStorage.removeItem('isLoggedIn')
+        setIsLoggedIn(false);
+    };
+
     return (
-        <div>
-            <p>React App</p>
-        </div>
+        <>
+            <MainHeader isAuthenticated={isLoggedIn} onLogout={logoutHandler} />
+            <main>
+                {!isLoggedIn && <Login onLogin={loginHandler} />}
+                {isLoggedIn && <Home onLogout={logoutHandler} />}
+            </main>
+        </>
     );
 }
 
