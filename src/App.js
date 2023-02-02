fb5f0ff3080c0c20185b6d13d4ba34a7195e5d98
thiagovb62@gmail.com <thiagovb62@gmail.com>
2023-02-02 15:52:56 -0300

    starting a new project react

--- src/App.js
+++ src/App.js
@@ -1,46 +1,15 @@
 import './App.css';
 import React, {useState,fragment} from 'react';
-import AddUser from "./components/User/AddUser";
-import UsersList from "./components/User/UsersList";
 
-const DUMMY_DATA = [
-    {
-        id: 1,
-        name: 'John',
-        age: 20,
-    },
-    {
-        id: 2,
-        name: 'Jane',
-        age: 21,
-    },
-    {
-        id: 3,
-        name: 'Jack',
-        age: 22,
-    }
-];
+
 
 function App() {
-    const [users, setUsers] = useState(DUMMY_DATA);
-    const addUserHandler = (uName,uAge) => {
-        setUsers((prevUsers) => {
-            return [...prevUsers,{name:uName, age:uAge, id:Math.random().toString()}];
-        });
-    }
-    // return <>
-    //     <header className="App-header">
-    //         <AddUser onAddUser={addUserHandler}/>
-    //         <UsersList users={users}/>
-    //     </header>
-    // </>;
 
-    return <fragment>
-        <header className="App-header">
-            <AddUser onAddUser={addUserHandler}/>
-            <UsersList users={users}/>
-        </header>
-    </fragment>;
+    return (
+        <div>
+            <p>React App</p>
+        </div>
+    );
 }
 
 export default App;
